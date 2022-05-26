1c4a70b3068bea0d0cab60df0d325662be60ee24
Vaishnavi Devi V <vaisuvenkat2000@gmail.com>
2022-05-26 14:25:59 +0530

    Add project files.

---
+++ CICD_MVC_Demo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CICD_MVC_Demo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
